d73ba76917514c4afab9e0c4b1290067898677e5
panaromafinance <124047482+panaromafinance@users.noreply.github.com>
2023-03-14 14:31:13 +0530

    leftover ETH fixed

--- contracts/PanaromaswapV1Router02.sol
+++ contracts/PanaromaswapV1Router02.sol
@@ -455,7 +455,7 @@ contract PanaromaswapV1Router02 is IPanaromaswapV1Router02 {
         require(path[0] == WETH, 'PanaromaswapV1Router: INVALID_PATH');
         uint amountIn = msg.value;
         IWETH(WETH).deposit{value: amountIn}();
-        assert(IWETH(WETH).transfer(PanaromaswapV1Library.pairFor(factory, path[0], path[1]), amountIn*995/10000));
+        assert(IWETH(WETH).transfer(PanaromaswapV1Library.pairFor(factory, path[0], path[1]), amountIn*995/1000));
         uint balanceBefore = IERC20(path[path.length - 1]).balanceOf(to);
         swapSupportingFeeOnTransferTokens(path, to);
         refPlanETHForTokenSupportingFee(path, amountIn);
