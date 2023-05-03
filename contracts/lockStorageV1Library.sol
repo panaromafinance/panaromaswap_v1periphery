db2f85c374996f89f357218677f343a026376a34
panaromafinance <124047482+panaromafinance@users.noreply.github.com>
2023-05-03 14:46:06 +0530

    Update lockStorageV1Library.sol

--- contracts/lockStorageV1Library.sol
+++ contracts/lockStorageV1Library.sol
@@ -15,15 +15,4 @@ library lockStorageV1Library {
         require(token0 != address(0), 'PanaromaswapV1Library: ZERO_ADDRESS');
     }
 
-    // calculates the CREATE2 address for a pair without making any external calls
-    function pairFor(address factory, address token0, address token1) internal pure returns (address pair) {
-        (address token0, address token1) = sortTokens(token0, token1);
-        pair = address(uint(keccak256(abi.encodePacked(
-                hex'ff',
-                factory,
-                keccak256(abi.encodePacked(token0, token1)),
-                hex'59678869e9ef16e3c8fb5e405499ae5c98535c043c0ad6ef3992db2402ec081b' // init code hash
-            ))));
-    }
-
 }
